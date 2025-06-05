3fed5e984b635b7ea02fcede00fc5aa3315419cb
marcoscode404 <viniciusmg99@outlook.com>
2025-06-05 00:19:22 -0400

    feat: add ententendo conceito de banco de dados, configurando o sqlalchemy com o sqlite

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key" ## onde o banco de dados vai ficar
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' ## caminho para o banco
+
+db = SQLAlchemy(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_word():
