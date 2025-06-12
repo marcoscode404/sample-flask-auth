fef8f50a6efa18d12d709800dda427b0db12696d
marcoscode404 <viniciusmg99@outlook.com>
2025-06-11 23:49:08 -0400

    feat: add integrando com o banco de dados

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
-from app import db
-class User(db.Model):
+from database import db
+from flask_login import UserMixin
+class User(db.Model, UserMixin):
     # Mapeamento de dados no banco de dados
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80, nullable=False, unique=True))
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
