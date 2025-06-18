60652acc7177bcf53ff166e93249dd3e1fd09d10
marcoscode404 <viniciusmg99@outlook.com>
2025-06-17 23:09:55 -0400

    feat: add sistema de autenticação

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key" ## onde o banco de dados vai ficar
@@ -12,8 +12,13 @@ db.init_app(app)
 login_manager.init_app(app)
 
 # view login
-
+login_manager.login_view = 'login' 
 # Session <- conexão ativa
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route('/login', methods=["POST"])
 def login():
     data = request.json
@@ -22,9 +27,11 @@ def login():
 
     if username and password:
         # Login
-        user = User.query.filter_by(username=username).firts()
+        user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
     
     return jsonify({"message": "Credenciais inválidas"}), 400
